f3aa8675a03b2be423aefa2dc3657d8bd37485ff
k3rne1.pan1c <philippklemmer.web@gmail.com>
2018-02-26 18:48:08 +0100

    added function to create a dataset, to write data to a file and save it as index.html, opens it in firefox

---
+++ 011_templates/main.go
@@ -0,0 +1,82 @@
+/*
+	--- Udemy Course ---
+	This is "Learn about Templates in Go Part 1" of the learning webdevelopment with GoLang
+	from GoesToEleven
+	https://github.com/GoesToEleven/golang-web-dev/tree/master/001_prereq
+*/
+package main
+
+import (
+	"fmt"
+	"time"
+	"os"
+	"os/exec"
+	"io"
+	"log"
+	"strings"
+)
+
+type User struct {
+	name string
+	timestamp string
+}
+
+func createDataSet(name string, timestamp string) string {
+	return `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>Welcome ` + name +`</title>
+		</head>
+		<body>
+			<h1>Welcome ` + name + `! Your logged in @` + timestamp +`
+		</body>
+		</html>
+	`
+}
+
+func writeDataToFile(filename string, data string) {
+	nf, err := os.Create(filename)
+	if err != nil {
+		log.Fatal("error creating file", err)
+	}
+	//always close file!
+	defer nf.Close()
+
+	// copy copies from src to destination until EOF is reached on source 
+	// or an error occurs
+	if _, err := io.Copy(nf, strings.NewReader(data)); err != nil {
+		log.Fatal("error while writing to file", err)
+	}
+}
+
+func main() {
+
+	// get current timestamp as string and set timezone to berlin
+	_ , err := time.LoadLocation("Europe/Berlin")
+	if err != nil {
+		fmt.Println("Couldn't read out local timezones", err)
+		return
+	}
+	t := time.Now().Format("Jan 2")
+
+	// create user for data
+	u := User{
+		name: "Philipp",
+		timestamp: t,
+	}
+
+	fmt.Println("Welcome to learning templating. #part1")
+
+	fmt.Println("Creating dataset")
+	data := createDataSet(u.name, u.timestamp)
+	fmt.Println(data)
+
+	fmt.Println("Write Data to file")
+	writeDataToFile("index.html", data)
+
+	fmt.Println("open new file in browser")
+	exec.Command("firefox", "index.html").Output()
+
+}
